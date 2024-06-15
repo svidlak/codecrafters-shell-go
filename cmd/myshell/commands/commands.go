2cb3ea0fc7521e91c626629a7ea258b6bab03570
Max Svidlo <svid320@gmail.com>
2024-06-15 14:37:15 +0300

    push

---
+++ cmd/myshell/commands/commands.go
@@ -0,0 +1,3 @@
+package commands
+
+var Commands_list = map[string]string{"name": "max"}
--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/codecrafters-io/shell-starter-go/cmd/myshell/commands"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		panic("bad input")
+	}
+	getInput(input)
+}
+
+func getInput(input string) {
+	val, ok := commands.Commands_list[input]
+
+	if !ok {
+		fmt.Println(input + ": command not found")
+		return
+	}
+
+	fmt.Println(val)
 }
